a4094c68fd70833264244439f98a14435962d0ca
Mārtiņš Lazdāns <marrtins@dqdp.net>
2025-03-12 11:41:33 +0200

    Remove declaring properties on interface

--- error.c
+++ error.c
@@ -6,17 +6,10 @@
 #include "pdo_firebird_utils.h"
 
 zend_class_entry *FireBird_IError_ce;
-static zend_object_handlers FireBird_IError_object_handlers;
-
-const zend_function_entry FireBird_IError_methods[] = {
-    PHP_FE_END
-};
 
 void register_FireBird_IError_ce()
 {
     zend_class_entry tmp_ce;
-    INIT_NS_CLASS_ENTRY(tmp_ce, "FireBird", "IError", FireBird_IError_methods);
+    INIT_NS_CLASS_ENTRY(tmp_ce, "FireBird", "IError", NULL);
     FireBird_IError_ce = zend_register_internal_interface(&tmp_ce);
-
-    DECLARE_ERR_PROPS(FireBird_IError_ce);
 }
