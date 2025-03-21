8fab7ded46aff2cd49da7388467f6f36d570c17c
Mārtiņš Lazdāns <marrtins@dqdp.net>
2025-03-21 09:23:05 +0200

    Combine args_*.c

--- args.c
+++ args.c
@@ -2,7 +2,7 @@
 #include "php.h"
 #include "php_firebird_includes.h"
 
-zend_class_entry *FireBird_Connect_Args_ce;
+zend_class_entry *FireBird_Connect_Args_ce, *FireBird_Create_Args_ce;
 
 void register_FireBird_Connect_Args_ce()
 {
@@ -12,4 +12,15 @@ void register_FireBird_Connect_Args_ce()
 
     DECLARE_PROP_STRING(FireBird_Connect_Args_ce, database, ZEND_ACC_PUBLIC);
     declare_props_zmap(FireBird_Connect_Args_ce, &database_connect_zmap);
+}
+
+void register_FireBird_Create_Args_ce()
+{
+    zend_class_entry tmp_ce;
+
+    INIT_NS_CLASS_ENTRY(tmp_ce, "FireBird", "Create_Args", NULL);
+    FireBird_Create_Args_ce = zend_register_internal_class(&tmp_ce);
+
+    DECLARE_PROP_STRING(FireBird_Create_Args_ce, database, ZEND_ACC_PUBLIC);
+    declare_props_zmap(FireBird_Create_Args_ce, &database_create_zmap);
 }
